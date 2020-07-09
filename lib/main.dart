4098cad521951e04dc1c48b28e7cebea681601f8
e00556236 <ekrem.hatipoglu@huawei.com>
2020-07-09 11:33:14 +0300

    comment

--- lib/main.dart
+++ lib/main.dart
@@ -29,14 +29,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   final Set<Marker> _markers = {};
   final Set<Polygon> _polygons = {};
   final Set<Polyline> _polylines = {};
   final Set<Circle> _circles = {};
 
   HuaweiMapController _mapController;
-
 
   @override
   Widget build(BuildContext context) {
@@ -144,45 +142,68 @@ class _MyHomePageState extends State<MyHomePage> {
   void _addMarker(LatLng location) {
     String markerId = "${location.lat}-${location.lng}";
     Marker marker = new Marker(
+      // Set Marker Id
       markerId: MarkerId(markerId),
+
+      // Marker Location
       position: location,
+
+      // InfoWindow. It will display after clicking the marker.
       infoWindow: InfoWindow(
         title: 'Title',
         snippet: 'Desc: $markerId',
       ),
+
+      // Add click listener
       clickable: true,
       onClick: () {
         print("Marker clicked: $markerId");
       },
+
+      // You can set icon from assets, bytes or default icon
       icon: BitmapDescriptor.defaultMarker,
     );
 
+    // Add marker to _markers list in setState. if you don't call it in setState
+    // your screen won't render again and marker won't display.
     setState(() {
       _markers.add(marker);
     });
   }
 
   void _drawPolygon() {
+    // We will draw polygon between markers. Therefore get marker locations
     List<LatLng> dots = [];
     _markers.forEach((element) => dots.add(element.position));
 
     Polygon polygon = new Polygon(
+        // Set Polygon Id
         polygonId: PolygonId("MyPolygon"),
+
+        // Set locations
         points: dots,
+
+        // Customize your polygon
         fillColor: Colors.green[500],
         strokeColor: Colors.green[900],
         strokeWidth: 5,
         zIndex: 2,
+
+        // Add click listener
         clickable: true,
         onClick: () {
           print("Polygon clicked");
         });
+
+    // Add polygon to _polygons list in setState. if you don't call it in setState
+    // your screen won't render again and polygon won't display.
     setState(() {
       _polygons.add(polygon);
     });
   }
 
   void _drawPolyine() {
+    // Define polyine locations
     List<LatLng> dots = [
       new LatLng(41.06071, 28.98772),
       new LatLng(37.918918340362126, 28.841235177965157),
@@ -197,31 +218,49 @@ class _MyHomePageState extends State<MyHomePage> {
     ];
 
     Polyline polyline = new Polyline(
+
+        // Set Polyline Id
         polylineId: PolylineId("MyPolyline"),
+
+        // Set locations
         points: dots,
+
+        // Customize your polyline
         color: Colors.red[500],
         zIndex: 2,
+
+        // Add click listener
         clickable: true,
         onClick: () {
           print("Polyline clicked");
         });
 
+    // Add polyline to _polylines list in setState. if you don't call it in setState
+    // your screen won't render again and polyline won't display.
     setState(() {
       _polylines.add(polyline);
     });
   }
 
   void _drawCircles() {
+    // Add circle to _circles list in setState. if you don't call it in setState
+    // your screen won't render again and circle won't display.
     setState(() {
       _circles.add(Circle(
+          // Set Circle Id
           circleId: CircleId('MyCircle'),
+          // Set locations
           center: LatLng(0, 0),
           radius: 3000,
+
+          // Customize your polyline
           visible: true,
           fillColor: Color.fromARGB(100, 100, 100, 0),
           strokeColor: Colors.blue,
           strokeWidth: 20,
           zIndex: 2,
+
+          // Add click listener
           clickable: true,
           onClick: () {
             print("Circle clicked");
